a41beb541c4aa5edc5800303f32c54aa16442cdb
Philip Schulz <phs@phisch.org>
2020-11-02 06:02:35 +0100

    SWO Output Support / Use ITM for debug prints.

--- main.cpp
+++ main.cpp
@@ -32,8 +32,13 @@
 #include <spi/SpiDevice.hpp>
 #include <devices/Ws2812bStrip.hpp>
 
+
+#include <stm32/Itm.hpp>
+#include <stm32/Tpi.hpp>
+#include <stm32/CoreDbg.hpp>
+
 /*******************************************************************************
- * System Devices
+ * PLL Configuration
  ******************************************************************************/
 static const constexpr stm32::PllCfg pllCfg = {
     .m_pllSource        = stm32::PllCfg::PllSource_t::e_PllSourceHSE,
@@ -48,6 +53,17 @@ static const constexpr stm32::PllCfg pllCfg = {
     .m_apb2Prescaler    = stm32::PllCfg::APBPrescaler_t::e_APBPrescaler_None
 };
 
+const uint32_t SystemCoreClock = pllCfg.getSysclkSpeedInHz();
+
+static_assert(pllCfg.isValid() == true,                            "PLL Configuration is not valid!");
+static_assert(SystemCoreClock               == 84 * 1000 * 1000,   "Expected System Clock to be at 84 MHz!");
+static_assert(pllCfg.getAhbSpeedInHz()      == 84 * 1000 * 1000,   "Expected AHB to be running at 84 MHz!");
+static_assert(pllCfg.getApb1SpeedInHz()     == 42 * 1000 * 1000,   "Expected APB1 to be running at 42 MHz!");
+static_assert(pllCfg.getApb2SpeedInHz()     == 84 * 1000 * 1000,   "Expected APB2 to be running at 84 MHz!");
+
+/*******************************************************************************
+ * System Devices
+ ******************************************************************************/
 static stm32::Scb                       scb(SCB);
 static stm32::Nvic                      nvic(NVIC, scb);
 
@@ -67,9 +83,6 @@ static gpio::GpioEngine                 gpio_engine_B(&gpio_B);
 static stm32::Gpio::C                   gpio_C(rcc);
 static gpio::GpioEngine                 gpio_engine_C(&gpio_C);
 
-static stm32::Gpio::D                   gpio_D(rcc);
-static gpio::GpioEngine                 gpio_engine_D(&gpio_D);
-
 /*******************************************************************************
  * LEDs
  ******************************************************************************/
@@ -77,96 +90,18 @@ static gpio::AlternateFnPin             g_mco1(gpio_engine_A, 8);
 static gpio::DigitalOutPin              g_led_green(gpio_engine_C, 13);
 
 /*******************************************************************************
- * UART
+ * SWO Trace via the Cortex M4 Debug Infrastructure 
  ******************************************************************************/
-static gpio::AlternateFnPin             uart_tx(gpio_engine_A, 3);
-static gpio::AlternateFnPin             uart_rx(gpio_engine_A, 2);
-static stm32::Uart::Usart2<gpio::AlternateFnPin>    uart_access(rcc, uart_rx, uart_tx);
-uart::UartDevice                        g_uart(&uart_access);
+static gpio::AlternateFnPin swo(gpio_engine_B, 3);
 
-/*******************************************************************************
- * WS2812b Strip
- ******************************************************************************/
-static gpio::AlternateFnPin spi_sclk(gpio_engine_A, 5);
-static gpio::AlternateFnPin spi_nsel(gpio_engine_A, 4);
-static gpio::AlternateFnPin spi_mosi(gpio_engine_B, 5);
-static gpio::AlternateFnPin spi_miso(gpio_engine_A, 6);
-
-#if defined(SPI_VIA_DMA)
-static stm32::Spi::DmaSpi1<
-    gpio::AlternateFnPin,
-    decltype(ws2812b_spiTxDmaChannel),
-    decltype(ws2812b_spiRxDmaChannel)
->
-#else
-static stm32::Spi::Spi1<
-    gpio::AlternateFnPin
->
-#endif
-ws2812b_spibus( /* p_rcc = */ rcc,
-            #if defined(SPI_VIA_DMA)
-                /* p_txDmaChannel = */ ws2812b_spiTxDmaChannel,
-                /* p_rxDmaChannel = */ ws2812b_spiRxDmaChannel,
-            #endif
-                /* p_sclk = */ spi_sclk,
-                /* p_nsel = */ spi_nsel,
-                /* p_mosi = */ spi_mosi,
-                /* p_miso = */ spi_miso,
-                /* p_prescaler = */ decltype(ws2812b_spibus)::BaudRatePrescaler_e::e_SpiPrescaler32
-);
-
-static spi::DeviceT ws2812b_spidev(&ws2812b_spibus);
-static devices::Ws2812bStripT<
-    17,
-    decltype(ws2812b_spidev),
-    devices::Ws2812bDataInverted
->                                                           ws2812bStrip(ws2812b_spidev);
+static stm32::CoreDbgT<CoreDebug_BASE>                          coreDbg;
+static stm32::TpiT<TPI_BASE, decltype(coreDbg), decltype(swo)>  tpi(coreDbg, swo);
+static stm32::ItmT<ITM_BASE, decltype(tpi)>                     itm(tpi, stm32::Itm::getDivisor(SystemCoreClock /*, 2'250'000 */));
 
 /*******************************************************************************
  * Tasks
  ******************************************************************************/
 static tasks::HeartbeatT<decltype(g_led_green)> heartbeat_gn("hrtbt_g", g_led_green, 3, 500);
-
-#if 1
-/* FIXME Only for testing the WS2812B Driver via SPI and DMA. */
-static bool led_status = false;
-
-int
-toggleLed(void *p_data) {
-    assert(p_data != nullptr);
-
-    bool *status = static_cast<bool *>(p_data);
-
-    Pixel::RGB color;
-    if (*status) {
-        color = Pixel::RGB(0x40'00'00);
-    } else {
-        color = Pixel::RGB(0x00'40'00);
-    }
-
-    for (unsigned idx = 0; idx < ws2812bStrip.SIZE; idx++) {
-        ws2812bStrip.setPixel(idx, color);
-    }
-    ws2812bStrip.show();
-
-    *status = !(*status);
-
-    return (0);
-}
-
-static tasks::PeriodicCallback  task_500ms("t_500ms", 2, 500, &toggleLed, &led_status);
-#endif
-
-/*******************************************************************************
- *
- ******************************************************************************/
-const uint32_t SystemCoreClock = pllCfg.getSysclkSpeedInHz();
-
-static_assert(pllCfg.isValid() == true,                            "PLL Configuration is not valid!");
-static_assert(SystemCoreClock               == 84 * 1000 * 1000,   "Expected System Clock to be at 84 MHz!");
-static_assert(pllCfg.getAhbSpeedInHz()      == 84 * 1000 * 1000,   "Expected AHB to be running at 84 MHz!");
-static_assert(pllCfg.getApb1SpeedInHz()     == 42 * 1000 * 1000,   "Expected APB1 to be running at 42 MHz!");
-static_assert(pllCfg.getApb2SpeedInHz()     == 84 * 1000 * 1000,   "Expected APB2 to be running at 84 MHz!");
 
 /*******************************************************************************
  *
@@ -179,7 +114,8 @@ int
 main(void) {
     rcc.setMCO(g_mco1, decltype(rcc)::MCO1Output_e::e_PLL, decltype(rcc)::MCOPrescaler_t::e_MCOPre_5);
 
-    uart_access.setBaudRate(decltype(uart_access)::BaudRate_e::e_230400);
+    /* FIXME This should go in the Object Tree somewhere */
+    DBGMCU->CR = /* DBGMCU_CR_DBG_SLEEP_Msk | DBGMCU_CR_DBG_STOP_Msk | DBGMCU_CR_DBG_STANDBY_Msk | */ DBGMCU_CR_TRACE_IOEN_Msk;
 
     const unsigned sysclk = pllCfg.getSysclkSpeedInHz() / 1000;
     const unsigned ahb    = pllCfg.getAhbSpeedInHz() / 1000;
@@ -201,6 +137,16 @@ bad:
     while (1) ;
 
     return (0);
+}
+
+void
+debug_printf(const char * const p_fmt, ...) {
+    va_list va;
+    va_start(va, p_fmt);
+
+    ::tfp_format(&itm, decltype(itm)::putf, p_fmt, va);
+
+    va_end(va);
 }
 
 #if defined(__cplusplus)
